86dc4622f9fb5bdbcb1ac1a6ea630094fdbf56c8
Renata Guerra <re.guerr@gmail.com>
2020-08-09 19:43:50 -0300

    WhattApp and BE API calls

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,34 +3,57 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
+
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  user_id: string;
+  whatsapp: string;
+}
 
 
-function TeacherItem () {
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> =({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id:teacher.id,
+    })
+  }
+
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/30126229?s=460&u=93ab4ea5fe6f2a59cc6fd14f0ce0839b1f67daa5&v=4" alt="Renata Guerra"/>
+        <img src={teacher.avatar}alt={teacher.name}/>
         <div>
-          <strong>Renata Guerra</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Pessoa dura por fora mas mole por dentro.
-        <br /><br />
-        Apaixonada por tudo que tem botões e luzinhas, adora um eletronico e se possível tem todos que existem na face da terra. Seria uma ótima blogueirinha mas né!
-      </p>
+      <p> {teacher.bio} </p>
 
       <footer>
         <p>
           Preço/Hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
